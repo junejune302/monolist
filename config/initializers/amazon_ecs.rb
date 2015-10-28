a52009f0cf13d42e7f25a062c56d81e61b505cab
junejune302 <junejune302@yahoo.co.jp>
2015-10-28 08:54:32 +0000

    first commit

---
+++ config/initializers/amazon_ecs.rb
@@ -0,0 +1,6 @@
+Amazon::Ecs.configure do |options|
+  options[:AWS_access_key_id] = 'AKIAJMRFQIIBPFUXO25A'
+  options[:AWS_secret_key]    = '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'
+  options[:associate_tag]     = 'junejune302-22'
+  options[:country] = 'jp'
+end
